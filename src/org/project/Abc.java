13f6b0ec70c3c63aa879095255af6225bbdb0fcb
surya27620 <suryaprakash1463@gmail.com>
2021-05-30 07:44:13 +0530

    my first push1

---
+++ src/org/project/Abc.java
@@ -0,0 +1,12 @@
+package org.project;
+
+public class Abc {
+	private void getDriver() {
+		System.out.println("enter the name");
+
+	}
+public static void main(String[] args) {
+	Abc v = new Abc();
+	v.getDriver();
+}
+}
